a555a50b09227de1f1486f977666a1fa087937ac
Mike Roylance <roylance.michael@gmail.com>
2013-12-14 14:52:57 -0700

    initial commit

---
+++ gameOfLifeTests.py
@@ -0,0 +1,12 @@
+import unittest
+import gameOfLife
+
+class GameOfLifeTesting(unittest.TestCase):
+	 def test_somethingsomething(self):
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+	main()
